4c93c37193865a3597b2c672eed39538f9edec4a
liaodehui <1959956996@qq.com>
2019-08-13 16:53:38 +0800

    汽车界面

---
+++ car/pages/index/index.js
@@ -0,0 +1,79 @@
+const db=require('../../assets/db.js')
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    slides: db.slides||[],
+    interval:5000,//过多少时间换
+    duration:500//换图片时间
+  },
+
+  readMore(e){
+    const id=e.target.dataset.id
+    wx.navigateTo({
+      url: '/pages/detail/index?id='+id,
+      success: (result)=>{
+        
+      },
+      fail: ()=>{},
+      complete: ()=>{}
+    });
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    // console.log(this.data.slides)
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})

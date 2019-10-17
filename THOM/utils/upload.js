979227585438b309d801a4c6eaf87e5ee99c09fc
liaodehui <1959956996@qq.com>
2019-10-17 13:02:30 +0800

    修改-封装url

--- THOM/utils/upload.js
+++ THOM/utils/upload.js
@@ -1,16 +1,15 @@
 var app =  getApp();
 
 wx.cloud.init()
-const db = wx.cloud.database();
 
-var upload = function(image_files){
+var upload = function(url,image_files){
     return new Promise((resolve,reject)=>{
       var images_fileID = []
       for(let i = 0; i < image_files.length; i++){
         var imageUrl = image_files[i].split('/')
         var name = imageUrl[imageUrl.length-1]
         wx.cloud.uploadFile({
-          cloudPath:'THOM/post_images/'+name,
+          cloudPath:`${url}${name}`,
           filePath:image_files[i],
           success:res => {
             images_fileID.push(res.fileID)
